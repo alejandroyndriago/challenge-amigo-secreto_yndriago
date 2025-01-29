de75c65b693fc57641436553f6b396377cd32337
Alejandro Yndriago <alejandroyndriagodiaz@gmail.com>
2025-01-28 21:26:47 -0500

    se creo funcion agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
+//Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+let amigo = document.getElementById("amigo");
+let listaAmigos = document.getElementById("listaAmigos");
+
+//Funcion para agregar amigo con el boton desde el DOM
+function agregarAmigo() {
+    if (amigo.value == "") {
+        alert("Por favor inserte un nombre");
+    } else {
+    nombresAmigos.push(amigo.value);
+    //Dejara el campo en blanco luego de agregar el nombre a la lista
+    amigo.value = "";
+    console.log(nombresAmigos);
+    }
+}
+
+
+function amigoEnPantalla() {
+
+    for (let index = 0; index < nombresAmigos.length; index++) {
+        const element = array[index];
+        
+    }
+}
